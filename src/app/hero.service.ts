253e5dd97110b402ec79a855474e3b9869ab089d
James Wong <wongwongjames@yahoo.com.hk>
2022-07-29 10:28:47 +0800

    add function to delete hero

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -39,6 +39,11 @@ export class HeroService {
     return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)), catchError(this.handleError<Hero>('addHero')));
   }
 
+  deleteHero(id: Number): Observable<Hero> {
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.delete<Hero>(url, this.httpOptions).pipe(tap(_ => this.log(`deleted hero id=${id}`)), catchError(this.handleError<Hero>('deleteHero')));
+  }
+
   private log(message: string) {
     this.messageService.add(`HeroService: ${message}`);
   }
